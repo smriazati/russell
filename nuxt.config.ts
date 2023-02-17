4648c901cae3c9ac27f31c7dc99d8c8b5aa1b045
smriazati <sarahriazati@gmail.com>
2023-02-17 15:22:18 -0500

    target static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
   modules: ['@nuxtjs/sanity'],
   app: {
     layoutTransition: { name: 'layout', mode: 'out-in' },
